b4d203f0f88a6d9329d10e676e3e500a03f82d2e
Ishan Gill <63602807+solarpy@users.noreply.github.com>
2023-01-24 15:53:53 +0800

    initial commit

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        DemiSwap
+      </Link>
+      <ul>
+        <CustomLink to="/payment">Payment</CustomLink>
+        <CustomLink to="/exchange">Exchange</CustomLink>
+        <CustomLink to="/swap">Swap</CustomLink>
+        <CustomLink to="/news">News</CustomLink>
+      </ul>
+    </nav>
+  )
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
